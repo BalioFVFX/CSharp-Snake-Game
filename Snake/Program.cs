19b8e8cc831c572d2734d21d1750f4223654b258
Erik Baliov <vfxbaliof@gmail.com>
2018-04-25 16:37:01 +0300

    Snake Game

---
+++ Snake/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Snake Game";
+
+            int snakeXPosition = 0;
+            int snakeYPosition = 0;
+
+            while (true)
+            {
+                Console.SetCursorPosition(snakeXPosition, snakeYPosition);
+                Console.Write("*");
+                ConsoleKey pressedKey = Console.ReadKey().Key;
+                if(pressedKey== ConsoleKey.DownArrow)
+                {
+                    snakeYPosition++;
+                    continue;
+                }
+                if(pressedKey == ConsoleKey.UpArrow && snakeYPosition != 0)
+                {
+                    snakeYPosition--;
+                    continue;
+                }
+                if(pressedKey == ConsoleKey.RightArrow)
+                {
+                    snakeXPosition++;
+                    continue;
+                }
+                if (pressedKey == ConsoleKey.LeftArrow && snakeXPosition != 0)
+                {
+                    snakeXPosition--;
+                    continue;
+                }
+               
+            }
+        }
+    }
+}
