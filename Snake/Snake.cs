7cc282c14fee11f77c1cee6029b75f3c0ef8e8a4
Erik Baliov <vfxbaliof@gmail.com>
2018-04-28 12:04:56 +0300

    Snake class

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading;
 
 namespace Snake
 {
@@ -10,9 +11,58 @@ namespace Snake
         {
             this.PositionX = x;
             this.PositionY = y;
+            this.SnakeTail = new List<Snake>();
         }
 
         public int PositionX;
         public int PositionY;
+        public List<Snake> SnakeTail;
+
+
+        public static bool CollisionWithSnake(List<Snake> snakeTail)
+        {
+            for (int i = 0; i < snakeTail.Count - 1; i++)
+            {
+                if (snakeTail[i].PositionX == snakeTail[snakeTail.Count - 1].PositionX && snakeTail[i].PositionY == snakeTail[snakeTail.Count - 1].PositionY)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public static void PlaySound()
+        {
+            Console.Beep(150, 100);
+        }
+
+        public static bool CollisionWithFood(List<Snake> snakeTail, int foodPositionX, int foodPositionY)
+        {
+            if (snakeTail[snakeTail.Count - 1].PositionX == foodPositionX && snakeTail[snakeTail.Count - 1].PositionY == foodPositionY)
+            {
+                Thread soundThread = new Thread(PlaySound);
+                Snake tail = new Snake(snakeTail[snakeTail.Count - 1].PositionX, snakeTail[snakeTail.Count - 1].PositionY);
+                soundThread.Start();
+                snakeTail.Add(tail);
+                return true;
+            }
+            return false;
+        }
+
+        public static List<Snake> InitializeSnake(int snakeLength)
+        {
+            List<Snake> snakeTail = new List<Snake>();
+            for (int i = 0; i < snakeLength; i++)
+            {
+                Snake snake = new Snake(31 + i, 3);
+                snakeTail.Add(snake);
+            }
+
+            Console.SetCursorPosition(31, 3);
+            Console.Write(new string('*', snakeLength));
+
+            return snakeTail;
+        }
+
     }
 }
