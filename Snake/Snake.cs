8825123ef3891d9292d3e7b1f749e7b88484b26b
Erik Baliov <vfxbaliof@gmail.com>
2018-05-02 17:03:48 +0300

    More snake colors

--- Snake/Snake.cs
+++ Snake/Snake.cs
@@ -68,28 +68,83 @@ namespace Snake
         public void SetColor()
         {
             Console.WriteLine("Select snake color 1 - 4");
-            Console.ForegroundColor = ConsoleColor.White;
-            Console.WriteLine("1 - White");
-            Console.ForegroundColor = ConsoleColor.Magenta;
-            Console.WriteLine("2 - Magenta");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1 - Blue");
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("2 - Cyan");
+            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.WriteLine("3 - Dark Blue");
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+            Console.WriteLine("4 - Dark Cyan");
+            Console.ForegroundColor = ConsoleColor.DarkGray;
+            Console.WriteLine("5 - Dark Gray");
             Console.ForegroundColor = ConsoleColor.DarkGreen;
-            Console.WriteLine("3 - Dark Green");
+            Console.WriteLine("6 - Dark Green");
+            Console.ForegroundColor = ConsoleColor.DarkMagenta;
+            Console.WriteLine("7 - Dark Magenta");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("8 - Dark Red");
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            Console.WriteLine("9 - Dark Yellow");
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.WriteLine("10 - Gray");
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("4 - Green");
+            Console.WriteLine("11 - Green");
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("12 - Magenta");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("13 - Red");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine("14 - White");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            Console.WriteLine("15 - Yellow");
             Console.ForegroundColor = ConsoleColor.White;
             switch (int.Parse(Console.ReadLine()))
             {
                 case 1:
-                    Color = ConsoleColor.White;
+                    Color = ConsoleColor.Blue;
                     break;
                 case 2:
-                    Color = ConsoleColor.Magenta;
+                    Color = ConsoleColor.Cyan;
                     break;
                 case 3:
-                    Color = ConsoleColor.DarkGreen;
+                    Color = ConsoleColor.DarkBlue;
                     break;
                 case 4:
+                    Color = ConsoleColor.DarkCyan;
+                    break;
+                case 5:
+                    Color = ConsoleColor.DarkGray;
+                    break;
+                case 6:
+                    Color = ConsoleColor.DarkGreen;
+                    break;
+                case 7:
+                    Color = ConsoleColor.DarkMagenta;
+                    break;
+                case 8:
+                    Color = ConsoleColor.DarkRed;
+                    break;
+                case 9:
+                    Color = ConsoleColor.DarkYellow;
+                    break;
+                case 10:
+                    Color = ConsoleColor.Gray;
+                    break;
+                case 11:
                     Color = ConsoleColor.Green;
+                    break;
+                case 12:
+                    Color = ConsoleColor.Magenta;
+                    break;
+                case 13:
+                    Color = ConsoleColor.Red;
+                    break;
+                case 14:
+                    Color = ConsoleColor.White;
+                    break;
+                case 15:
+                    Color = ConsoleColor.Yellow;
                     break;
                 default:
                     Color = ConsoleColor.Gray;
